88c3d501e4a446207929f1f61fdc13dfd86ebe65
Vlad Gromadtsov <vlad.gromadtsov@gmail.com>
2022-05-08 18:15:33 +0400

    First commit

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task1 // Note: actual namespace depends on the project name.
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 4;
+
+            for (; a < 10; a++)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
