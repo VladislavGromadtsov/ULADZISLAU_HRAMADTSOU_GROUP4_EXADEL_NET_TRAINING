60557d5d51e8011a197c2c00c4c1001f1fa200bf
Vlad Gromadtsov <vlad.gromadtsov@gmail.com>
2022-05-24 20:09:44 +0400

    Task5 solution

---
+++ Task5/Task5/ApplicationContext.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task5.Models;
+
+namespace Task5
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var subject1 = new Subject() { Id = 1, Name = "Math" };
+            var subject2 = new Subject() { Id = 2, Name = "Biology" };
+            var subject3 = new Subject() { Id = 3, Name = "Chemistry" };
+            var subject4 = new Subject() { Id = 4, Name = "Phisics" };
+
+            var class1 = new Class() { Id = 1, Number = 11, Letter = "A" };
+            var class2 = new Class() { Id = 2, Number = 10, Letter = "B" };
+            var class3 = new Class() { Id = 3, Number = 9, Letter = "C" };
+            var class4 = new Class() { Id = 4, Number = 8, Letter = "D" };
+
+            var student1 = new Student() { Id = 1, FirstName = "Ivan", LastName = "Ivanov", PhoneNumber = "375293522222", Address = "Gomel", ClassId = 1 };
+            var student2 = new Student() { Id = 2, FirstName = "Vasya", LastName = "Vasiliev", PhoneNumber = "375293522223", Address = "Minsk", ClassId = 2 };
+            var student3 = new Student() { Id = 3, FirstName = "Kostya", LastName = "Kostilev", PhoneNumber = "375293521223", Address = "Grodno", ClassId = 3 };
+            var student4 = new Student() { Id = 4, FirstName = "Max", LastName = "Maxov", PhoneNumber = "375293511223", Address = "Gomel", ClassId = 4 };
+
+            modelBuilder.Entity<Class>().HasData(
+                new Class[]
+                {
+                    class1, class2, class3, class4
+                });
+
+            modelBuilder.Entity<Student>().HasData(
+                new Student[]
+                {
+                    student1, student2, student3, student4
+                });
+
+            modelBuilder.Entity<Subject>().HasData(
+                new Subject[]
+                {
+                    subject1, subject2, subject3, subject4
+                });
+
+            modelBuilder.Entity<Class>()
+                .HasMany(s => s.Subjects)
+                .WithMany(c => c.Classes)
+                .UsingEntity(j => j
+                .ToTable("ClassSubject")
+                .HasData(new
+                {
+                    ClassesId = 1, SubjectsId = 2
+                }));
+
+            modelBuilder.Entity<Class>()
+                .HasMany(s => s.Subjects)
+                .WithMany(c => c.Classes)
+                .UsingEntity(j => j
+                .ToTable("ClassSubject")
+                .HasData(new
+                {
+                    ClassesId = 2,
+                    SubjectsId = 3
+                }));
+
+            modelBuilder.Entity<Class>()
+                .HasMany(s => s.Subjects)
+                .WithMany(c => c.Classes)
+                .UsingEntity(j => j
+                .ToTable("ClassSubject")
+                .HasData(new
+                {
+                    ClassesId = 3,
+                    SubjectsId = 4
+                }));
+
+            modelBuilder.Entity<Class>()
+                .HasMany(s => s.Subjects)
+                .WithMany(c => c.Classes)
+                .UsingEntity(j => j
+                .ToTable("ClassSubject")
+                .HasData(new
+                {
+                    ClassesId = 4,
+                    SubjectsId = 1
+                }));
+        }
+    }
+}
---
+++ Task5/Task5/Migrations/20220524152340_DataAdding.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Task5.Migrations
+{
+    public partial class DataAdding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Classes",
+                columns: new[] { "Id", "Letter", "Number" },
+                values: new object[,]
+                {
+                    { 1, "A", (byte)11 },
+                    { 2, "B", (byte)10 },
+                    { 3, "C", (byte)9 },
+                    { 4, "D", (byte)8 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Subjects",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Math" },
+                    { 2, "Biology" },
+                    { 3, "Chemistry" },
+                    { 4, "Phisics" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ClassSubject",
+                columns: new[] { "ClassesId", "SubjectsId" },
+                values: new object[,]
+                {
+                    { 1, 2 },
+                    { 2, 3 },
+                    { 3, 4 },
+                    { 4, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Address", "ClassId", "FirstName", "LastName", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { 1, "Gomel", 1, "Ivan", "Ivanov", "375293522222" },
+                    { 2, "Minsk", 2, "Vasya", "Vasiliev", "375293522223" },
+                    { 3, "Grodno", 3, "Kostya", "Kostilev", "375293521223" },
+                    { 4, "Gomel", 4, "Max", "Maxov", "375293511223" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ClassSubject",
+                keyColumns: new[] { "ClassesId", "SubjectsId" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "ClassSubject",
+                keyColumns: new[] { "ClassesId", "SubjectsId" },
+                keyValues: new object[] { 2, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "ClassSubject",
+                keyColumns: new[] { "ClassesId", "SubjectsId" },
+                keyValues: new object[] { 3, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "ClassSubject",
+                keyColumns: new[] { "ClassesId", "SubjectsId" },
+                keyValues: new object[] { 4, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Classes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Classes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Classes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Classes",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Subjects",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Subjects",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Subjects",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Subjects",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
